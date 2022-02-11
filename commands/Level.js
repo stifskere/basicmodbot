edd36f28d120ef5623d9d4b21dead54e867329fb
Stif <stifskere@gmail.com>
2022-02-11 02:28:01 +0100

    Working on level stuff

--- commands/Level.js
+++ commands/Level.js
@@ -1,10 +1,25 @@
 module.exports = {
     name: "level",
     execute(message, args, config, moment, RichEmbed, date, embeds, bot, db){
-       const member = message.author;
-        const levelembed = new RichEmbed()
-            .setColor(config.Embedcolor)
-            .setTitle(`Your level is:`)
 
-    }
+        let user = message.content.split(" ")[1];
+        if(!user){
+            user = message.author.id;
+        }
+        console.log(user);
+            let member = user.toString().replace(/[\\<>@#&!']/g, "")
+        db.all(`SELECT * FROM levelstable WHERE UserID = ?`, [member], (err, row) =>{
+            if(err){console.log(err);message.channel.send(err7); return;}
+            row.forEach( function (rows){
+                let messages = rows.messages
+                let level = rows.level
+                const levelembed = new RichEmbed()
+                    .setColor(config.Embedcolor)
+                    .setTitle(``)
+                    .addField('Mention', `<@${member.toString()}>`)
+                    .setDescription(`Level: ${level} Messages: ${messages}`)
+                message.channel.send(levelembed)
+            })
+
+    })}
 }
